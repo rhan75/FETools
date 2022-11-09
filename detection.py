32c40ab8febcff5ab53be53ef5903f7057edd907
rhan75 <rhan75@gmail.com>
2022-11-08 23:45:25 -0800

    Refactored / Removed global where not needed

--- detection.py
+++ detection.py
@@ -8,29 +8,27 @@ from datetime import datetime
 
 config = configparser.ConfigParser()
 config.read('./config.ini')
-api_key = config['DEFAULT']['API_Key']
-conn = None
-
+API_KEY = config['DEFAULT']['API_Key']
 filename = None
 result = None
 
-def select_file():
+def insert_text(textbox, contents): #Change textbox from readonly to normal, clear, insert contents if exists, and disable edit
+    textbox.configure(state='normal')
+    textbox.delete('1.0', 'end')
+    if contents:
+        textbox.insert('1.0', contents)
+    textbox.configure(state='disabled')
+
+def select_file(): #Select the file for testing
     global filename
     filename = askopenfilename()
-    text_file.configure(state='normal') #Change file textbox to normal to enable writing 
-    text_file.delete('1.0','end') #Clean file textbox
-    text_file.insert('1.0',filename) #Update file textbox with the filename
-    text_file.configure(state='disabled') #Disable edit on file textbox
+    insert_text(text_file, filename)
 
-def check_file():
-    global conn
+def check_file(): #Check for malicious code from a selected file, then get the report back
     global result
-    global filename
     result = None
+    conn = fireeyepy.Detection(key=API_KEY)
     if filename:
-        if not conn:
-            #Todo: Try except to handle error
-            conn = fireeyepy.Detection(key=api_key)
         report = get_report(filename, conn)
         basic_result = (
             f"Report ID: {report['report_id']}\n"
@@ -53,12 +51,9 @@ def check_file():
             )
     else: 
         result = 'Failed - Select a file to submit first'
-    text_result.configure(state='normal')
-    text_result.delete('1.0', 'end')
-    text_result.insert('1.0', result)
-    text_result.configure(state='disabled')
+    insert_text(text_result, result)
         
-def get_report(filename, conn):
+def get_report(filename, conn): #Submit filename and return the report
     response = conn.submit_file(
         files={
             'file':('filename', open(filename, 'rb'))
@@ -67,8 +62,6 @@ def get_report(filename, conn):
     return conn.get_report(response['report_id'])
 
 def save_result(): #Save result into a text file
-    global result
-    global filename
     if not result:
         save_status = 'Cannot Save the result - No result found'
 
@@ -80,22 +73,16 @@ def save_result(): #Save result into a text file
         with open(save_file, 'w') as file:
             file.write(result)
         save_status = f'{save_file} created'
-    text_result.configure(state='normal')
-    text_result.delete('1.0', 'end')
-    text_result.insert('1.0', save_status)
-    text_result.configure(state='disabled')
+    insert_text(text_result, save_status)
 
 def clear_text(): #Clear all textboxes
     global result
     global filename
     filename = None
     result = None
-    text_file.configure(state='normal')
-    text_file.delete('1.0','end')
-    text_file.configure(state='disabled')
-    text_result.configure(state='normal')
-    text_result.delete('1.0','end')
-    text_result.configure(state='disabled')
+    insert_text(text_file, filename)
+    insert_text(text_result, result)
+
 root = tk.Tk()
 frame = ttk.Frame(root, padding=10)
 frame.grid()
@@ -103,7 +90,7 @@ root.title('Detection on Demand')
 button_frame = ttk.Frame(frame)
 button_frame.grid(column=0, row=2, sticky='W')
 
-
+#Textbox for file and result
 text_file = tk.Text(frame, state='disabled', height=1)
 text_file.grid(column=0, row=0, columnspan=3, sticky='W', pady=5, ipadx=2, ipady=2)
 
@@ -113,8 +100,6 @@ text_result.grid(column=0, row=1, columnspan=4, sticky='W', pady=10)
 #Adding Browse Button
 button_browse = ttk.Button(frame, text="Browse", command=select_file)
 button_browse.grid(column=3, row=0, sticky='W')
-
-
 
 #Adding Submit / Export / Clear / Close buttons
 button_submit = ttk.Button(button_frame, text='Submit', command=check_file)
